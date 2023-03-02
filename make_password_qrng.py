f986e37589f03683814714dc353028702c24d5bb
Asad <asadhasan@Asads-MacBook-Pro.local>
2023-03-02 08:16:24 -0600

    Quantum random number generator based password generator.

---
+++ make_password_qrng.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import random
+import string
+import urllib.request
+ 
+password_length = int(input("Enter the length of password:"))
+def generatingPassword(quantum_seed):
+    password = random.sample(quantum_seed, password_length)
+
+    password += random.choice(string.printable)
+ 
+    passwordList = list(password)
+    random.shuffle(passwordList)
+    password = "".join(passwordList)
+     
+    return password
+
+req = urllib.request.Request('https://qrng.anu.edu.au/wp-content/plugins/colours-plugin/get_block_alpha.php')
+with urllib.request.urlopen(req) as response:
+   quantum_seed_block = response.read()
+   print(f"Password of length {password_length}: {generatingPassword(quantum_seed_block.decode('ascii'))}")
