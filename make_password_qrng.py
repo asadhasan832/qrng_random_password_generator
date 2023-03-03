1db7528c99472122f945e1d19847951fd6a5fc90
Asad <asadhasan@Asads-MacBook-Pro.local>
2023-03-03 08:00:53 -0600

    Update comments and variable renaming.

--- make_password_qrng.py
+++ make_password_qrng.py
@@ -4,10 +4,11 @@ import string
 import urllib.request
 
 password_length = int(input("Enter the length of password:"))
-def generatingPassword(quantum_seed):
-    password = random.sample(quantum_seed, password_length)
+def generatingPassword(quantum_seed_block):
+    password = random.sample(quantum_seed_block, password_length)
 
-    password += random.choice(string.printable)
+    password += random.choice(string.ascii_uppercase)
+    password += random.choice(string.digits)
  
     passwordList = list(password)
     random.shuffle(passwordList)
@@ -15,6 +16,7 @@ def generatingPassword(quantum_seed):
      
     return password
 
+# Attain a quantum random character block seed
 req = urllib.request.Request('https://qrng.anu.edu.au/wp-content/plugins/colours-plugin/get_block_alpha.php')
 with urllib.request.urlopen(req) as response:
    quantum_seed_block = response.read()
