8d917a35a7e1fb1142ede3dbfc5f3511ee98eab9
Asad <root@Asads-MBP.hsd1.tx.comcast.net>
2023-04-29 09:33:38 -0500

    Ignore CA check due to second psuedo random and crashing.

--- make_password_qrng.py
+++ make_password_qrng.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import random
 import string
+import ssl
 import urllib.request
 
 password_length = int(input("Enter the length of password:"))
@@ -18,6 +19,10 @@ def generatingPassword(quantum_seed_block):
 
 # Attain a quantum random character block seed
 req = urllib.request.Request('https://qrng.anu.edu.au/wp-content/plugins/colours-plugin/get_block_alpha.php')
-with urllib.request.urlopen(req) as response:
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+with urllib.request.urlopen(req, context=ctx) as response:
    quantum_seed_block = response.read()
+   #print(quantum_seed_block)
    print(f"Password of length {password_length}: {generatingPassword(quantum_seed_block.decode('ascii'))}")
